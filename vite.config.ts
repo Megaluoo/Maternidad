03e7fb2a288a4635a89de31f7c8dce0f9bf00355
Megaluoo <155444060+Megaluoo@users.noreply.github.com>
2026-02-12 13:26:59 -0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // CAMBIA 'nombre-de-tu-repo' POR EL NOMBRE REAL DE TU REPOSITORIO
-  // Ejemplo: si tu repo es https://github.com/usuario/mi-web, pon '/mi-web/'
-  base: '/Maternidad/',
+  // ESTA LÍNEA ES LA CLAVE PARA QUE NO SALGA EN BLANCO EN GITHUB PAGES
+  base: "/Maternidad/",
 
   plugins: [
     react(),
